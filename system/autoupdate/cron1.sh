3520170d47804d10ed392f992e4a09a6fadf5753
Linus Niederer <linus.niederer@bluemail.ch>
2021-03-03 11:09:26 +0100

    Update cron1.sh

--- system/autoupdate/cron1.sh
+++ system/autoupdate/cron1.sh
@@ -3,6 +3,7 @@
 # Configuration - keepalived
 # @author: https://github.com/linusniederer
 # @link: https://github.com/linusniederer/ubuntu-configurations/edit/main/system/autoupdate/cron1.sh
+# @raw: https://raw.githubusercontent.com/linusniederer/ubuntu-configurations/main/system/autoupdate/cron1.sh
 #
 
 # ********************* INIT
