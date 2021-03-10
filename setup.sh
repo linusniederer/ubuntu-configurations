12bafdd76a726e2aee3e69d22b95c993c9e035dd
Linus Niederer <linus.niederer@bluemail.ch>
2021-03-10 15:11:18 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+
+# ubuntu-configurations
+#
+# @author: https://github.com/linusniederer
+# @link: https://github.com/linusniederer/ubuntu-configurations/blob/main/setup.sh
+# @raw: https://raw.githubusercontent.com/linusniederer/ubuntu-configurations/main/setup.sh
+#
 
